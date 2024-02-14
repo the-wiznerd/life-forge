75b2fd0cc4ecf4112ebe41858c6a07eeeeacb0b9
Brett Cooper <brett@thewiznerd.com>
2024-02-14 15:31:53 -0700

    Got deck working.

--- components/AppCard.vue
+++ components/AppCard.vue
@@ -1,7 +1,38 @@
 <template>
-  <div :class="['card', `card--face-${isFaceUp ? 'up' : 'down'}`]">
+  <div
+    :class="[
+      'card',
+      `card--face-${isFaceUp ? 'up' : 'down'}`,
+      `stat-category--${card.statCategory}`
+    ]"
+  >
     <div v-if="!isFaceUp" class="card__back"></div>
-    <div v-else class="card__face">Card</div>
+    <div v-else class="card__face">
+      <div class="card__header">
+        <h2 class="card__title">{{ card.title }}</h2>
+        <div class="card__value">{{ card.value }}<small>XP</small></div>
+      </div>
+      <div class="card__body">
+        <div class="card__description">
+          <p>{{ card.description }}</p>
+          <p></p>
+        </div>
+        <div v-if="card.bonuses" class="card__bonuses">
+          <p v-for="(bonus, index) in card.bonuses" :key="index">
+            <span class="card-bonus">
+              <span
+                :class="`card-bonus__value stat-category--${bonus.statCategory}`"
+              >
+                {{ bonus.value }}<small>XP</small>
+              </span>
+              <span class="card-bonus__description">
+                &nbsp;{{ bonus.description }}
+              </span>
+            </span>
+          </p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
--- components/screen/Play.vue
+++ components/screen/Play.vue
@@ -1,10 +1,11 @@
 <template>
   <div class="screen screen--play">
-    <div class="card-deck">
+    <div v-if="shuffledCards.length" class="card-deck" @click="shuffle">
       <AppCard
-        v-for="card in store.incompleteCards"
+        v-for="card in shuffledCards"
         :key="card.id"
         :card="card"
+        :is-face-up="card.id === topCard?.id"
       />
     </div>
   </div>
@@ -12,4 +13,18 @@
 
 <script setup lang="ts">
   const store = useStore()
+
+  const shuffledCards = computed(() => shuffleArray(store.incompleteCards))
+
+  const topCard = computed(() =>
+    shuffledCards.value.length
+      ? shuffledCards.value[shuffledCards.value.length - 1]
+      : null
+  )
+
+  function shuffle() {
+    if (topCard.value) {
+      store.completedCards.push(topCard.value)
+    }
+  }
 </script>
